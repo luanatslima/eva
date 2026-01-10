3788c74b736956dbeb39b6264c569b8a3291ad84
luanatslima <luanatayna2007@gmail.com>
2026-01-10 18:47:54 -0300

    classes definidas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
+    
+class clubeDeLivro(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    data_fundacao = models.DateField()
+
+    def __str__(self):
+        return self.nome
+    
+class Editor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nome
+    
+class Escritor(models.Model):
+    nome = models.CharField(max_length=100)
+    biografia = models.TextField()
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+    
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    resumo = models.TextField()
+    data_publicacao = models.DateField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    escritor = models.ForeignKey(Escritor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
